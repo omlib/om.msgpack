f5730f136889b42296939da64d56e3dda9d7816c
tong <tong@disktree.net>
2025-07-02 18:02:10 +0200

    initial port of msgpack-haxe library

---
+++ src/om/Msgpack.hx
@@ -0,0 +1,18 @@
+package om;
+
+import haxe.io.Bytes;
+import om.msgpack.Decoder.DecodeOption;
+
+class Msgpack {
+	public static inline function encode(obj:Dynamic):Bytes {
+		return new om.msgpack.Encoder().encode(obj);
+	}
+
+	public static inline function decode(bytes:Bytes, ?option:DecodeOption):Dynamic {
+		return new om.msgpack.Decoder().decode(bytes, option);
+	}
+
+	public static inline function decodeMap(bytes:Bytes):Map<String, Dynamic> {
+		return new om.msgpack.Decoder().decode(bytes, AsMap);
+	}
+}
