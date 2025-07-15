de7f40dea6b40a03a85c2d8b2e1bd8e5f0b7a515
tong <tong@disktree.net>
2025-07-15 12:58:23 +0200

    add documentation, remove decodeMap

--- src/om/Msgpack.hx
+++ src/om/Msgpack.hx
@@ -4,15 +4,17 @@ import haxe.io.Bytes;
 import om.msgpack.Decoder.DecodeOption;
 
 class Msgpack {
+	/**
+		Encode an object to msgpack
+	**/
 	public static inline function encode(obj:Dynamic):Bytes {
 		return new om.msgpack.Encoder().encode(obj);
 	}
 
+	/**
+		Decode msgpack bytes to object (or map)
+	**/
 	public static inline function decode(bytes:Bytes, ?option:DecodeOption):Dynamic {
 		return new om.msgpack.Decoder(option).decode(bytes);
-	}
-
-	public static inline function decodeMap(bytes:Bytes):Map<String, Dynamic> {
-		return new om.msgpack.Decoder(AsMap).decode(bytes);
 	}
 }
